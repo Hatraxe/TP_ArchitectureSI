41f92e3f99e590bcab99055cc032eefdf7e0c079
Louis Soulier <soulierlouis@hotmail.fr>
2024-04-17 15:06:56 +0200

    camarhce

--- store_online/src/main/java/fr/univtours/polytech/store_online/dao/UserDAOImplJPA.java
+++ store_online/src/main/java/fr/univtours/polytech/store_online/dao/UserDAOImplJPA.java
@@ -19,7 +19,7 @@ public class UserDAOImplJPA implements UserDAO{
     @SuppressWarnings("unchecked") // Pour éviter les warnings de cast
     @Override
     public List<Userbean> getAllUsers() {
-        Query requete = em.createNativeQuery("SELECT * FROM boutique.user", Userbean.class);
+        Query requete = em.createNativeQuery("SELECT * FROM Userbean", Userbean.class);
         return requete.getResultList();
     }
 
@@ -30,7 +30,7 @@ public class UserDAOImplJPA implements UserDAO{
 
     @Override
     public boolean checkUserCredentials(String username, String password) {
-        TypedQuery<Userbean> query = em.createQuery("SELECT u FROM User u WHERE u.username = :username AND u.password = :password", Userbean.class);
+        TypedQuery<Userbean> query = em.createQuery("SELECT u FROM Userbean u WHERE u.login = :username AND u.password = :password", Userbean.class);
         query.setParameter("username", username);
         query.setParameter("password", password);
         return !query.getResultList().isEmpty();
