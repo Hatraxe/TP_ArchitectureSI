b42d641a6a54624a52a659e301efd24d684bac54
Louis Soulier <soulierlouis@hotmail.fr>
2024-04-17 12:29:24 +0200

    sevelt

--- store_online/src/main/java/fr/univtours/polytech/store_online/controller/LoginServlet.java
+++ store_online/src/main/java/fr/univtours/polytech/store_online/controller/LoginServlet.java
@@ -2,6 +2,7 @@ package fr.univtours.polytech.store_online.controller;
 
 import java.io.IOException;
 
+import fr.univtours.polytech.store_online.model.Userbean;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -14,12 +15,27 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // Récupération de la liste des résultats
-
-        
-        // Redirection vers la page de gestion des notes.
-        request.getRequestDispatcher("login.jsp").forward(request, response);
-
+          request.getRequestDispatcher("login.jsp").forward(request, response);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Récupérer les données du formulaire
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+    
+        
+        // Créer un bean de user
+        Userbean userbean = new Userbean();
+        userbean.setLogin(username);
+        userbean.setLogin(password);
+        
+        
+        
+        // Si bon mot de passe et nom d'utilisateur alors rediriger vers la page de la lsite des articles
+        response.sendRedirect("articles");
+
+        //Sinon afficher le message d'erreur en rouge "Le nom d'utilisateur ou le mot de passe saisit n'est pas valide."
+
+    }
 }
---
+++ store_online/src/main/java/fr/univtours/polytech/store_online/controller/ShowArticlesListServlet.java
@@ -0,0 +1,9 @@
+package fr.univtours.polytech.store_online.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "ShowArticlesListServlet", urlPatterns = "/articlesS")
+public class ShowArticlesListServlet extends HttpServlet {
+
+}
